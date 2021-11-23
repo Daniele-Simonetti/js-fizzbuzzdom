992bae6dc4da4e582fcb67bc32427f6deb883543
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-23 15:29:20 +0100

    stamp numbers from 0 to 100 on console

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // scrivere un programma che stampi in console i numeri da 1 a 100;
+for (let RowNumber = 1; RowNumber <= 100; RowNumber++) {
+  console.log(RowNumber);
+}
 // 1.Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 // 2. Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
