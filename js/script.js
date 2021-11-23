cbb865722d82a7c38728ce2baf1ee02db480cc31
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-23 17:18:38 +0100

    adding new colors and change innerHTML

--- js/script.js
+++ js/script.js
@@ -19,6 +19,42 @@ for (let RowNumber = 1; RowNumber <= 100; RowNumber++) {
 }
 
 // 2. Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+const list = document.querySelector('ul.number_list');
+for (let RowNumber = 1; RowNumber <= 100; RowNumber++) {
+  // se il numero è moltiplo di 3 e 5 mostra fizzbuzz 
+  if ((RowNumber % 3 == 0) && (RowNumber % 5 == 0)) {
+    const ItemList = document.createElement('li');
+    ItemList.append(RowNumber);
+    // cambio del testo
+    ItemList.innerHTML = 'fizzbuzz';
+    // aggiunta del colore 
+    ItemList.classList.add('red');
+    list.append(ItemList);
+    // se il numero è divisibile per 3 mostra fizz
+  } else if (RowNumber % 3 == 0) {
+    const ItemList = document.createElement('li');
+    ItemList.append(RowNumber);
+    // cambio del testo
+    ItemList.innerHTML = 'fizzbuzz';
+    // aggiunta del colore 
+    ItemList.classList.add('green');
+    list.append(ItemList);
 
-// 3. Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+    // se il numero è divisibile per 5 mostra buzz
+  } else if (RowNumber % 5 == 0) {
+    const ItemList = document.createElement('li');
+    ItemList.append(RowNumber);
+    // aggiunta del colore 
+    ItemList.classList.add('yellow');
+    list.append(ItemList);
+
+    // se il numero non soddisfa le condizioni sopra, allora mostra il numero 
+  } else {
+    const ItemList = document.createElement('li');
+    ItemList.append(RowNumber);
+    list.append(ItemList);
+  }
+}
+
+
 
